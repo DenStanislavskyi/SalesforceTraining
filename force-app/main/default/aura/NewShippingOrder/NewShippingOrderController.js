6dad45c8f7c402e8e591574399dbcff6d23ca8c1
Denys Stanislavskyi <Denys.Stanislavskyi@hlag.com>
2022-01-27 13:04:55 +0100

    Create New shipping order component

---
+++ force-app/main/default/aura/NewShippingOrder/NewShippingOrderController.js
@@ -0,0 +1,11 @@
+({
+    myAction : function(component, event, helper) {
+
+    },
+    
+    handleClick : function (cmp, event, helper) {
+        $A.get("e.force:closeQuickAction").fire();
+        $A.get("e.force:refreshView").fire();
+    }
+
+})
