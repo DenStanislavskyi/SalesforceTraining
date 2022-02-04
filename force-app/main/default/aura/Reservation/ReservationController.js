3b05acfaa0257062c8269161c3be85cc3aea0e29
Denys Stanislavskyi <Denys.Stanislavskyi@hlag.com>
2022-02-04 15:27:43 +0100

    ReservationComponent

---
+++ force-app/main/default/aura/Reservation/ReservationController.js
@@ -0,0 +1,12 @@
+({
+	myAction : function(component, event, helper) {
+		
+	},
+	handleClick : function (cmp, event, helper) {
+        $A.get("e.force:closeQuickAction").fire();
+        $A.get("e.force:refreshView").fire();
+    }
+
+
+})
+
